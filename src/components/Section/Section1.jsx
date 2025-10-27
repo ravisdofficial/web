a4842a3948c0183c339981355dc090a5670a8311
ravisdofficial <ravisdofficial@gmail.com>
2025-10-28 03:35:52 +0530

    main changes

--- src/components/Section/Section1.jsx
+++ src/components/Section/Section1.jsx
@@ -14,17 +14,11 @@ export default function Section1() {
     const [ref, isInView] = useScrollAnimation();
     return (
         <>
-            <div ref={ref} className='section-container' style={{
-                '--bg1': `url(${BG1})`,
-                '--bg2': `url(${BG2})`
-            }}>
+            <div ref={ref} className='section-container pt-[60px] vector-leaf-left vector-leaf-right' >
                 <p className='font-maharlika section__heading'>Our vision. Your dream.
                     <br />A perfect blend.</p>
-                <p className='font-barlow section__content' style={{
-                    '--bg3': `url(${BG3})`
-                }}>
+                <p className='font-barlow section__content' >
                     <motion.p className='text-[#6D3518] font-bold'
-
                         initial="hidden"
                         animate={isInView ? "visible" : "hidden"}
                         variants={slideFromLeft()}>
@@ -42,6 +36,13 @@ export default function Section1() {
                         From our origins in managed farmlands to a diverse portfolio that now includes exquisite villas and serene resorts, we connect individuals with luxury, sustainability, and nature.
                     </motion.p>
                 </p>
+                <motion.p className='font-maharlika mt-8 section__heading' 
+                        initial="hidden"
+                        animate={isInView ? "visible" : "hidden"}
+                        variants={slideFromLeft()} >
+                            <img src={BG3} />
+                            
+                </motion.p>
                 <motion.p className='font-maharlika mt-8 section__heading' 
                         initial="hidden"
                         animate={isInView ? "visible" : "hidden"}
